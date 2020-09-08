3df17251e1c7966a2efdb8a0a8682384e02abf8e
iamkryptonite <iamkryptonite@users.noreply.github.com>
2020-09-08 12:02:59 +0530

    fixed data for graphs

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,7 @@ import { Table,Row,Col } from 'antd'
 import './Home.css'
 import SideMenu from '../../constants/SideMenu'
 import CanvasJSReact from '../../canvasjs.react';
-var CanvasJS = CanvasJSReact.CanvasJS;
+// var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 const columns = [
     {
@@ -46,40 +46,35 @@ const columns = [
       },
     },
   ];
-  
 var data=[];
 var options = {
-    
   animationEnabled: true,
-  title:{
-    text: ""
-    
-  },
-  axisX: {
-    // valueFormatString: "MMM"
-  },
-  axisY: {
-    // title: "Sales (in USD)",
-    // prefix: "$"
-  },
-  data: [{
-    // yValueFormatString: "$#,###",
-    // xValueFormatString: "MMMM",
-    type: "spline",
-    markerColor:"#bf422c",
-    lineColor:"#bf422c",
-    dataPoints: this.state.deaths
-  }]
+			title:{
+				text: "Monthly Sales - 2017"
+			},
+			axisX: {
+				valueFormatString: "MMM"
+			},
+			axisY: {
+				title: "Sales (in USD)",
+				// prefix: "$"
+			},
+			data: [{
+				// yValueFormatString: "#####",
+				// xValueFormatString: "MMMM",
+				type: "spline",
+        dataPoints: ""
+      }]
 }
 class Home extends React.Component{
-    state={recovered:[],deaths:[],active:[]}        
+    state={recovered:[],deaths:[],active:[],options:options}
     componentDidMount(){
       axios.get('https://api.covid19india.org/data.json').then(res=>{
       this.setState({
         statewise:res.data.statewise,
-        casestimeseries:res.data.casestimeseries
+        casestimeseries:res.data.cases_time_series
       })
-      var key=1;  
+      var key=1;
       this.state.statewise.forEach(el => {
         if(el.statecode!=='TT'){
           data.push({
@@ -88,35 +83,37 @@ class Home extends React.Component{
             active:parseInt(el.active),
             confirmed:parseInt(el.confirmed),
             deaths:parseInt(el.deaths),
-            recovered:parseInt(el.recovered)  
+            recovered:parseInt(el.recovered)
           })
-        }         
+        }
       })
       var t=1;
       this.state.casestimeseries.forEach(el => {
-        if(t<=20){
-          this.state.active.push({x:el.date,y:el.totalconfirmed});
-          this.state.deaths.push({x:el.date,y:el.totaldeceased});
-          this.state.recovered.push({x:el.date,y:el.totalrecovered});
+          this.state.active.push({x:t,y:parseInt(el.totalconfirmed)});
+          this.state.deaths.push({x:t,y:parseInt(el.totaldeceased)});
+          this.state.recovered.push({x:t,y:parseInt(el.totalrecovered)});
           t++;
-        }
-        
-
+          if(t>20){
+            this.state.active.shift();
+            this.state.deaths.shift();
+            this.state.recovered.shift();
+          }
       })
-      // options.data.dataPoints=this.state.deaths;
     }).catch(err=>{
       console.log(err);
-    }).then(()=>{
+    })
+    .then(()=>{
       this.setState({data:data});
-      console.log(this.state.data)
-    }) 
+      options.data[0].dataPoints=this.state.deaths;
+      this.setState({options:options});
+      console.log(this.state.options);
+    })
     }
     onChange(pagination, filters, sorter, extra) {
       console.log('params', pagination, filters, sorter, extra);
     }
     render(){
-      
-        return(
+       return(
             <>
               <SideMenu/>
               <main>
@@ -127,9 +124,9 @@ class Home extends React.Component{
                     </div>
                   </Col>
                   <Col span={12}>
-                    <CanvasJSChart options = {options}/>
+                    <CanvasJSChart options = {this.state.options}/>
                     <br/>
-                    <CanvasJSChart options = {options}/>
+                    <CanvasJSChart options = {this.state.options}/>
                     <br/>
                     <CanvasJSChart options = {options}/>
                   </Col>
